8047b25a4fbd1c5b2bc75dd98b7abeee1e89eb92
ziu7wx <jin.zhu5@cn.bosch.com>
2023-05-15 15:35:37 +0800

    chore: ignore customized setups

---
+++ vat/conf/template_powercycle_setup.py
@@ -0,0 +1,23 @@
+SLOT_1_POWERCYCLE = {
+    "steps": {
+        "StepTest": {"enabled": True, "name": "maple"},
+        "StepCheckPowerCycle": {"enabled": True, "type": "network"},
+        "CheckStartupTrace": {"enabled": False},
+        "StepCheckRamdump": {"enabled": False},
+        "StepCheckCrash": {
+            "enabled": False,
+            "ex_filters": [],
+            "stop_filters": ["audio_service.core"],
+        },
+        "StepCheckProcesses": {"enabled": False, "qvm": "tmp/linux-la.config"},
+        "StepCheckLogdataStatus": {"enabled": False},
+        "StepCheckDisplays": {
+            "enabled": True,
+            "displays": [
+                {"index": 1, "profile": "Android_Home"},
+                {"index": 0, "profile": "Beantech_Home"},
+                {"index": 0, "profile": "Cluster"}
+            ]
+        },
+    }
+}
