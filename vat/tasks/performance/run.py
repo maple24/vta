00859c30cdbee7c6b7dc865d1ee90ffa919b3b7e
ziu7wx <jin.zhu5@cn.bosch.com>
2023-07-10 17:15:08 +0800

    feat: performance run

--- vat/tasks/performance/run.py
+++ vat/tasks/performance/run.py
@@ -13,16 +13,33 @@ logger.add(
 if __name__ == "__main__":
     nfs_disk = ["/data/vendor/nfs/mount", "/data/vendor/nfs/nfs_log"]
     aos_cmds = [
-        {"cmd": "top -b -n 1", "output": "aos_top_log"},
-        {"cmd": "dumpsys cpuinfo", "output": "aos_sys_cpuinfo.log"},
-        {"cmd": "ps -A", "output": "aos_ps.log"},
-        {"cmd": "cat /proc/cpuinfo", "output": "aos_proc_cpuinfo.log"},
-        {"cmd": "dumpsys meminfo", "output": "aos_meminfo.log"},
+        {"cmd": "top -b -n 1", "file": "aos_top_log"},
+        {"cmd": "dumpsys cpuinfo", "file": "aos_sys_cpuinfo.log"},
+        {"cmd": "ps -A", "file": "aos_ps.log"},
+        {"cmd": "cat /proc/cpuinfo", "file": "aos_proc_cpuinfo.log"},
+        {"cmd": "dumpsys meminfo", "file": "aos_meminfo.log"},
     ]
     qnx_cmds = [
-        {"cmd": "top -b -i 1", "output": "qnx_top.log"},
-        {"cmd": "hogs -i 1", "output": "qnx_hogs.log"},
-        {"cmd": "showmem -s", "output": "qnx_showmem.log"},
+        {"cmd": "top -b -i 1", "file": "qnx_top.log"},
+        {"cmd": "hogs -i 1", "file": "qnx_hogs.log"},
+        {"cmd": "showmem -s", "file": "qnx_showmem.log"},
     ]
-    mp = Performance(deviceID="", comport="", username="zeekr", password="Aa123123")
-    mp.android_nfs_iospeed
+    mp = Performance(deviceID="c35364f", comport="com5", username="zeekr", password="Aa123123")
+    # android nfs
+    for i in nfs_disk:
+        mp.android_nfs_iospeed(disk=i, type='w')
+        mp.android_nfs_iospeed(disk=i, type='r')
+    
+    # android ufs
+    mp.android_ufs_iospeed()
+
+    # qnx ufs: blocked
+    
+    # android cpu mem
+    for i in aos_cmds:
+        mp.qnx_cpu_mem(cmd=i['cmd'], file=i['file'])
+    
+    # qnx cpu mem
+    for i in qnx_cmds:
+        mp.qnx_cpu_mem(cmd=i['cmd'], file=i['file'])
+    
