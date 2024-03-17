ecade4fa8276c20ae9c3dc07577e2de51a884c1d
Jin Zhu <jin.zhu5@cn.bosch.com>
2024-03-17 21:07:32 +0800

    feat: add pytest to basic checks

--- vta/core/runner/function.py
+++ vta/core/runner/function.py
@@ -1,81 +1,53 @@
-# ============================================================================================================
-# C O P Y R I G H T
-# ------------------------------------------------------------------------------------------------------------
-# \copyright (C) 2024 Robert Bosch GmbH. All rights reserved.
-# ============================================================================================================
-
 import argparse
-import os
 import sys
 import time
 from datetime import datetime
-
 from loguru import logger
 from robot import run_cli
 from utils import find_file, rotate_folder
+from pathlib import Path
 
-# User argument interface
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", type=str, required=True, help="robot task file")
-parser.add_argument("--slot", default=1, type=int, help="slot number")
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", type=str, required=True, help="robot task file")
+    parser.add_argument("--slot", default=1, type=int, help="slot number")
+    parser.add_argument("--name", default="Functional Test", type=str, help="name of combined test suite")
+    parser.add_argument("--listener", default="FunctionListener.py", type=str, help="listener file")
+    parser.add_argument("--modifier", type=str, help="modifier file")
+    return parser.parse_args()
 
-parser.add_argument(
-    "--name", default="Functional Test", type=str, help="name of combined test suite"
-)
-parser.add_argument(
-    "--listener", default="FunctionListener.py", type=str, help="listener file"
-)
-parser.add_argument("--modifier", type=str, help="modifier file")
-args = parser.parse_args()
+def main():
+    logger.info("!!Start running functional test!!")
+    args = parse_arguments()
 
-TASK = args.task  # required
-SLOT = args.slot  # default to be 1
-REPORTNAME = args.name  # default to be `Functional Test`
-LISTENER = args.listener  # default to be `FunctionListener.py`
-MODIFIER = args.modifier  # default to be None
+    # ====================================DO NOT CHANGE====================================
+    ROOT = Path(__file__).resolve().parent.parent.parent.parent
+    LOG_PATH = ROOT / "log" / f"{datetime.now().strftime('%A')}_{time.strftime('%m%d%Y_%H%M')}_SLOT{args.slot}"
+    TASK_PATH = find_file(ROOT / "vta" / "tasks", args.task)
+    LISTENER_PATH = ROOT / "vta" / "core" / args.listener if args.listener else None
+    MODIFIER_PATH = ROOT / "vta" / "core" / args.modifier if args.modifier else None
+    logger.remove()
+    logger.add(sys.stdout, level="DEBUG")
+    logger.add(f"{LOG_PATH}/log_{time.strftime('%m%d%Y_%H%M%S')}.log",
+               backtrace=True, diagnose=False,
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="1 week",
+               level="TRACE")
+    rotate_folder(ROOT / "log")
+    # ====================================DO NOT CHANGE====================================
 
-"""
-=============================== DO NOT CHANGE ===============================
-"""
-ROOT = "\\".join(os.path.abspath(__file__).split("\\")[:-4])
-LOGPATH = os.path.join(
-    ROOT,
-    "log",
-    f"{datetime.now().strftime('%A')}_{time.strftime('%m%d%Y_%H%M')}_SLOT{SLOT}",
-)
-TASKPATH = find_file(os.path.join(ROOT, "vta", "tasks"), TASK)
-if LISTENER:
-    LISTENERPATH = os.path.join(ROOT, "vta", "core", LISTENER)
-if MODIFIER:
-    MODIFIERPATH = os.path.join(ROOT, "vta", "core", MODIFIER)
-logger.remove()
-logger.add(sys.stdout, level="DEBUG")
-mylogger = logger.add(
-    f"{LOGPATH}\\log_{time.strftime('%m%d%Y_%H%M%S')}.log",
-    backtrace=True,
-    diagnose=False,
-    format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-    rotation="1 week",
-    level="TRACE",
-)
-rotate_folder(os.path.join(ROOT, "log"))
-"""
-=============================== DO NOT CHANGE ===============================
-"""
+    if not TASK_PATH.exists():
+        logger.error(f"{args.task} not exist!")
+        sys.exit(1)
 
-# Function Runner
-if not os.path.exists(TASKPATH):
-    logger.error(f"{TASK} not exist!")
-    exit(1)
+    common = ["--exitonfailure", "--outputdir", str(LOG_PATH)]
+    if LISTENER_PATH:
+        common.extend(["--listener", str(LISTENER_PATH)])
+    if MODIFIER_PATH:
+        common.extend(["--prerunmodifier", str(MODIFIER_PATH)])
+    variable = ["--variable", f"SLOT:SLOT_{args.slot}"]
+    run_cli(common + variable + ["--exclude", "skip", str(TASK_PATH)], exit=True)
 
-common = [
-    "--exitonfailure",
-    "--outputdir",
-    f"{LOGPATH}",
-]
-if LISTENER:
-    common += ["--listener", f"{LISTENERPATH}"]
-if MODIFIER:
-    common += ["--prerunmodifier", f"{MODIFIERPATH}"]
-variable = ["--variable", f"SLOT:SLOT_{SLOT}"]
-rc = run_cli(common + variable + ["--exclude", "skip", f"{TASKPATH}"], exit=True)
+
+if __name__ == "__main__":
+    main()
